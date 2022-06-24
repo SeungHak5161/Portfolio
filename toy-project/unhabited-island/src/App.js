86cdadee1684c649835480b407d63c25563185f7
SeungHak5161 <bsh93367@gmail.com>
2022-06-24 23:50:30 +0900

    unhabited_island mainApp 구현중
    .

--- toy-project/unhabited-island/src/App.js
+++ toy-project/unhabited-island/src/App.js
@@ -1,8 +1,9 @@
 import "./Style/main.css";
 import { useEffect, useState } from "react";
+import MainApp from "./Component/MainApp";
 
 function App() {
-  const [state, setState] = useState([]);
+  const [data, setData] = useState([]);
   return (
     <div className="App">
       <header>
@@ -17,11 +18,12 @@ function App() {
             식수, 침대, 화장실, 세면시설, 쓰레기 처리장치만이 기본적으로
             제공되며, <b>30포인트</b>로 추가적인 물건이나 시설을 제공받을 수
             있습니다.
-            <br />(<b>100억 대신 50억</b>을 받는 조건으로 <b>5포인트</b>를 더
-            받을 수 있습니다.)
+            <br />(<b>10억</b>을 포기할 때 마다 <b>1포인트</b>를 더 얻을 수
+            있습니다.)
           </p>
         </article>
         <section id="main-app">
+          <MainApp data={data} onClick={() => {}} />
           <section id="control-bar"></section>
         </section>
       </main>
---
+++ toy-project/unhabited-island/src/Component/MainApp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function MainApp(props) {
+  const data = props.data;
+  const onClick = props.onClick;
+  return <div>MainApp</div>;
+}
