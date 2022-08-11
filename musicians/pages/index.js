169471d0bbe614f9dc283f31a6330464203d9e8f
SeungHak5161 <bsh93367@gmail.com>
2022-08-11 23:27:42 +0900

    main 작업중
    swiper 적용중

--- musicians/pages/index.js
+++ musicians/pages/index.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Layout from "../components/layout";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function Home() {
   return (
@@ -12,17 +14,56 @@ export default function Home() {
         <title>My Favorite Musicians</title>
         <meta name="description" content="SH's Favorite Musicians" />
       </Head>
-      {/* <section>
-        <Image
-          src="/images/kimsuyoung.jpg"
-          className={utilStyles.borderCircle}
-          width={"100%"}
-          height={"100%"}
-        ></Image>
+      <section>
+        {/* <div className={styles.homeDivImage}>
+          <Image
+            src="/images/kimsuyoung.jpg"
+            layout="fill"
+            objectFit="cover"
+          ></Image>
+        </div> */}
+        {/* <div className="swiper-wrapper"> */}
+        <Swiper
+          className={styles.swiperWrapper}
+          // direction={"horizontal"}
+          // autoplay={true}
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide className={styles.swiperImg}>
+            {/* <Image
+              // className={styles.swiperImg}
+              src="/images/kimsuyoung.jpg"
+              layout="fill"
+              objectFit="cover"
+            ></Image> */}
+            1111111111
+          </SwiperSlide>
+          <SwiperSlide className={styles.swiperImg}>
+            {/* <Image
+              // className={styles.swiperImg}
+              src="/images/kimsuyoung.jpg"
+              layout="fill"
+              objectFit="cover"
+            ></Image> */}
+            2222222222
+          </SwiperSlide>
+          <SwiperSlide className={styles.swiperImg}>
+            {/* <Image
+              // className={styles.swiperImg}
+              src="/images/kimsuyoslideung.jpg"
+              layout="fill"
+              objectFit="cover"
+            ></Image> */}
+            333333333333
+          </SwiperSlide>
+        </Swiper>
+        {/* </div> */}
         <h1 className={`${utilStyles.heading} ${utilStyles.bold15X}`}>
           나만 알기 아까운 가수들을 소개합니다.
         </h1>
-      </section> */}
+      </section>
     </Layout>
   );
 }
