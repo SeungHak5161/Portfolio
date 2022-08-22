dd6faccb644ac50dcbc3b48b6ae21f697ee2474f
SeungHak5161 <bsh93367@gmail.com>
2022-08-22 23:25:57 +0900

    Update releases.js
    에러난거 수정

--- musicians/next.config.js
+++ musicians/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["musicmeta-phinf.pstatic.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
