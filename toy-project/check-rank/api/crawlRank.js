fb8e2039d4fab3707c97bce1189d6f29c9a90f2c
Baek SeungHak <bsh93367@gmail.com>
2022-10-25 15:52:54 +0900

    Update crawlRank.js
    cors anywhere 변경

--- toy-project/check-rank/api/crawlRank.js
+++ toy-project/check-rank/api/crawlRank.js
@@ -1,6 +1,6 @@
 export default async function eplRank({ id, option }) {
   const url =
-    "https://cors-anywhere.herokuapp.com/https://api.football-data.org/v4/";
+    "https://cors-proxy-sh.herokuapp.com/https://api.football-data.org/v4/";
   const headers = { "X-Auth-Token": "d9760c70f8ab4cd8a93883383c336793" };
 
   /** get foot-ball data */
